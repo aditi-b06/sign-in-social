f2e37c083dd41127a48ff85419d818062f4a101c
aditi-b06 <74101063+aditi-b06@users.noreply.github.com>
2021-06-15 22:19:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   }
 function signout() {
+  alert("signing out");
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
