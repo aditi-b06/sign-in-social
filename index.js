3a5081f27466fa691e07d381c26e16810c82ee3f
aditi-b06 <74101063+aditi-b06@users.noreply.github.com>
2021-06-15 22:22:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ sign_in_btn.addEventListener('click',()=>{
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
      $("g.signin2").css("display","none");
+  $("container").css("display","none");
     $(".data").css("display","block");
     $("#pic").attr('src',profile.getImageUrl());
     $("#email").text(profile.getEmail());
@@ -24,6 +25,8 @@ function onSignIn(googleUser) {
   }
 function signout() {
   alert("signing out");
+  $("data").css("display","none");
+  $("container").css("display","block");
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
